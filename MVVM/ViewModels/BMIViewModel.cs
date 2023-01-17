bd6e4abada42e63c0769fa0b413b359caf5982be
Svetoslav Uzunov <svetoslavuzunov99@gmail.com>
2023-01-17 10:07:38 +0200

    Completed BMICalculator app

---
+++ MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BMICalculator.MVVM.Models;
+
+namespace BMICalculator.MVVM.ViewModels;
+
+public class BMIViewModel
+{
+	public BMIViewModel()
+	{
+		BMI = new BMI
+		{
+			Height = 180,
+			Weight = 73
+		};
+	}
+
+	public BMI BMI { get; set; }
+}
